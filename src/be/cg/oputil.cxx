sicortex/compiler

commit b25a04150e25cb342e45f8dc802f5459bc95f073

Fix the following intrinsics: __builtin_ia32_loadlpd __builtin_ia32_loadhpd __builtin_ia32_loadlps __builtin_ia32_loadhps
Use correct mov instruction for TOP_ldlp*, TOP_stlp*

src/be/cg/oputil.cxx
@@ -828,7 +828,7 @@ Mk_OP(TOP opr, ...)
 #endif // Is_True_On
 
 #ifdef TARG_X8664
-  if ( TOP_is_vector_high_loadstore ( OP_code ( op ) ) )
+  if ( TOP_is_vector_high_loadstore ( OP_code ( op ) ) || TOP_is_vector_lo_loadstore(OP_code(op)) )
     Set_OP_cond_def_kind(op, OP_ALWAYS_COND_DEF);
 #endif
   return op;

src/be/cg/x8664/expand.cxx
@@ -7363,9 +7363,11 @@ Exp_Intrinsic_Op (INTRINSIC id, TN *result, TN *op0, TN *op1, TN *op2, TN *op3, 
     Build_OP( TOP_for128v64, result, op0, op1, ops );
     break;
   case INTRN_LOADLPD:
-    Build_OP( TOP_ldsd, result, op1, Gen_Literal_TN (0,4), ops );
+    Build_OP( TOP_movapd, result, op0, ops );
+    Build_OP( TOP_ldlpd, result, op1, Gen_Literal_TN (0,4), ops );
     break;
   case INTRN_LOADHPD:
+    Build_OP( TOP_movapd, result, op0, ops );
     Build_OP( TOP_ldhpd, result, op1, Gen_Literal_TN (0,4), ops );
     break;
   case INTRN_UNPCKLPD:
@@ -7588,9 +7590,11 @@ Exp_Intrinsic_Op (INTRINSIC id, TN *result, TN *op0, TN *op1, TN *op2, TN *op3, 
     Build_OP( TOP_ldupd, result, op0, Gen_Literal_TN (0,4), ops );
     break;
   case INTRN_LOADHPS:
+    Build_OP( TOP_movaps, result, op0, ops );
     Build_OP( TOP_ldhps, result, op1, Gen_Literal_TN (0,4), ops );
     break;
   case INTRN_LOADLPS:
+    Build_OP( TOP_movaps, result, op0, ops );
     Build_OP( TOP_ldlps, result, op1, Gen_Literal_TN (0,4), ops );
     break;
   case INTRN_MOVMSKPS:

src/be/cg/x8664/cgemit_targ.cxx
@@ -1276,18 +1276,18 @@ static void Init_OP_Name()
   OP_Name[TOP_ldlps_n32] = "movlps";
   OP_Name[TOP_stlps] = "movlps";
   OP_Name[TOP_stlps_n32] = "movlps";
-  OP_Name[TOP_ldlpd] = "movsd";
-  OP_Name[TOP_ldlpd_n32] = "movsd";
-  OP_Name[TOP_stlpd] = "movsd";
-  OP_Name[TOP_stlpd_n32] = "movsd";
+  OP_Name[TOP_ldlpd] = "movlpd";
+  OP_Name[TOP_ldlpd_n32] = "movlpd";
+  OP_Name[TOP_stlpd] = "movlpd";
+  OP_Name[TOP_stlpd_n32] = "movlpd";
   OP_Name[TOP_ldlpsx] = "movlps";
   OP_Name[TOP_stlpsx] = "movlps";
-  OP_Name[TOP_ldlpdx] = "movsd";
-  OP_Name[TOP_stlpdx] = "movsd";
+  OP_Name[TOP_ldlpdx] = "movlpd";
+  OP_Name[TOP_stlpdx] = "movlpd";
   OP_Name[TOP_ldlpsxx] = "movlps";
   OP_Name[TOP_stlpsxx] = "movlps";
-  OP_Name[TOP_ldlpdxx] = "movsd";
-  OP_Name[TOP_stlpdxx] = "movsd";
+  OP_Name[TOP_ldlpdxx] = "movlpd";
+  OP_Name[TOP_stlpdxx] = "movlpd";
   OP_Name[TOP_ldhps] = "movhps";
   OP_Name[TOP_ldhps_n32] = "movhps";
   OP_Name[TOP_sthps] = "movhps";

commit b2651878f7d994d9eb4525185f48e76b2121028e

Fix regression caused by b25a04150e25cb342e45f8dc802f5459bc95f073

src/be/cg/oputil.cxx
@@ -828,7 +828,7 @@ Mk_OP(TOP opr, ...)
 #endif // Is_True_On
 
 #ifdef TARG_X8664
-  if ( TOP_is_vector_high_loadstore ( OP_code ( op ) ) || TOP_is_vector_lo_loadstore(OP_code(op)) )
+  if ( TOP_is_vector_high_loadstore ( OP_code ( op ) ) )
     Set_OP_cond_def_kind(op, OP_ALWAYS_COND_DEF);
 #endif
   return op;

src/be/cg/x8664/expand.cxx
@@ -7365,6 +7365,7 @@ Exp_Intrinsic_Op (INTRINSIC id, TN *result, TN *op0, TN *op1, TN *op2, TN *op3, 
   case INTRN_LOADLPD:
     Build_OP( TOP_movapd, result, op0, ops );
     Build_OP( TOP_ldlpd, result, op1, Gen_Literal_TN (0,4), ops );
+    Set_OP_cond_def_kind(ops, OP_ALWAYS_COND_DEF);
     break;
   case INTRN_LOADHPD:
     Build_OP( TOP_movapd, result, op0, ops );
@@ -7596,6 +7597,7 @@ Exp_Intrinsic_Op (INTRINSIC id, TN *result, TN *op0, TN *op1, TN *op2, TN *op3, 
   case INTRN_LOADLPS:
     Build_OP( TOP_movaps, result, op0, ops );
     Build_OP( TOP_ldlps, result, op1, Gen_Literal_TN (0,4), ops );
+    Set_OP_cond_def_kind(ops, OP_ALWAYS_COND_DEF);
     break;
   case INTRN_MOVMSKPS:
     Build_OP( TOP_movmskps, result, op0, ops );